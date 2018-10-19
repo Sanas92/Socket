3bb76373df4c06f929383a8a2b9a129063cac64f
Sanas92 <fantasygang92@gmail.com>
2018-10-19 17:47:17 +0900

    remove directory

--- RealTimeServerVer02/routes/chat.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/', (req, res) => {
-	res.render('webTalk', {userName : req.body.userName, userId : req.body.userId});
-});
-
-module.exports = router;
